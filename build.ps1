34b0bc78709e81679ab4076a067448c7612ae3ae
s2quake <han0210@netsgo.com>
2020-08-29 22:17:14 +0900

    빌드 스크립트 추가 및 서브모듈 업데이트

---
+++ build.ps1
@@ -0,0 +1,19 @@
+$location = Get-Location
+try {
+    Set-Location $PSScriptRoot
+    $buildFile = "./.vscode/build.ps1"
+    $propsPath = (
+        "./JSSoft.Library/Directory.Build.props",
+        "./JSSoft.Library.Commands/Directory.Build.props",
+        "./JSSoft.ModernUI.Framework/Directory.Build.props",
+        "./JSSoft.Font/Directory.Build.props"
+    ) | ForEach-Object { "`"$_`"" }
+    $propsPath = $propsPath -join ","
+    $solutionPath = "./font-generator.sln"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/s2quake/build/master/build.ps1" -OutFile $buildFile
+    Invoke-Expression "$buildFile $solutionPath $propsPath $args"
+}
+finally {
+    Remove-Item ./.vscode/build.ps1
+    Set-Location $location
+}
